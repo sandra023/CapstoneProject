aa53d6e8775934d20a26db3ded8bcf4f5232ef9b
Sandra Abernathy <sandraabernathy@Sandras-MacBook-Pro.local>
2019-09-05 10:47:45 -0600

    api

---
+++ src/main/java/EventApp/MyController.java
@@ -0,0 +1,12 @@
+package EventApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping("/")
+    public String hello(){
+        return "hello out there";
+    }
+}
